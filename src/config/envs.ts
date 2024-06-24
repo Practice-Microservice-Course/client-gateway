e42e64626e048aae6042a17e9aea1ddc9a924b4d
Nethz Jimenez <bynethz@gmail.com>
2024-06-23 22:51:24 -0500

    chore: Add initial project configuration files and modules

---
+++ src/config/envs.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import * as Joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  PRODUCTS_MICROSERVICE_PORT: number;
+  PRODUCTS_MICROSERVICE_HOST: string;
+}
+
+const envVarsSchema = Joi.object({
+  PORT: Joi.number().default(3000),
+  PRODUCTS_MICROSERVICE_PORT: Joi.number().default(3001),
+  PRODUCTS_MICROSERVICE_HOST: Joi.string().default('localhost'),
+}).unknown(true);
+
+const { error, value } = envVarsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  PORT: envVars.PORT,
+  PRODUCTS_MICROSERVICE_PORT: envVars.PRODUCTS_MICROSERVICE_PORT,
+  PRODUCTS_MICROSERVICE_HOST: envVars.PRODUCTS_MICROSERVICE_HOST,
+};
---
+++ src/products/products.controller.ts
@@ -0,0 +1,85 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: any) {
+    return this.productsClient.send(
+      { cmd: 'create_product' },
+      createProductDto,
+    );
+  }
+
+  @Get()
+  getAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find_all_products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async getProduct(@Param('id') id: string) {
+    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+
+    //Another way to handle errors
+
+    // try {
+    //   const product = await firstValueFrom(
+    //     this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+    //   );
+
+    //   return product;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id') id: string, @Body() updateProductDto: any) {
+    return this.productsClient
+      .send(
+        { cmd: 'update_product' },
+        {
+          id,
+          ...updateProductDto,
+        },
+      )
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') id: string) {
+    return this.productsClient.send({ cmd: 'remove_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+}
