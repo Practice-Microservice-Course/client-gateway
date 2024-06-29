3fc2813f3d05b25f8ee9c2b5a1e37734b7795a1c
Nethz Jimenez <bynethz@gmail.com>
2024-06-28 23:00:23 -0500

    chore: Update exception filter to use 'status' property instead of 'statusCode'

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -1,3 +1,4 @@
 export { CreateOrderDto } from './create-order.dto';
 export { OrderPaginationDto } from './order-pagination.dto';
 export { StatusDto } from './status.dto';
+export { OrderItemDto } from './order-item.dto';
